0a04066c2ef913d10cea546cce78b56f0da700a0
Emil <emilbabazade29@gmail.com>
2026-04-20 20:19:38 +0300

    player death

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,1 +1,13 @@
 extends Node2D
+
+@onready var player: Player = $Player
+
+func _ready() -> void:
+	HealthManager.death.connect(on_death)
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("restart"):
+		get_tree().reload_current_scene()
+
+func on_death() -> void:
+	player.queue_free.call_deferred()
--- scenes/saw/saw.gd
+++ scenes/saw/saw.gd
@@ -1,2 +1,6 @@
 extends Area2D
 class_name Saw
+
+
+func _on_body_entered(_body: Node2D) -> void:
+	HealthManager.death.emit()
